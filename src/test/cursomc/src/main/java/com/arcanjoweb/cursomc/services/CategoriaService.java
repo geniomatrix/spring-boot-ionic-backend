6bf8b5fb1ae2b9f00677c46e79c6bdcf7890479c
ArcanjoWeb <geniomatrix@hotmail>
2019-11-17 22:06:04 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/test/cursomc/src/main/java/com/arcanjoweb/cursomc/services/CategoriaService.java
+++ src/test/cursomc/src/main/java/com/arcanjoweb/cursomc/services/CategoriaService.java
@@ -16,7 +16,7 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		if (obj == null) {
 			throw new ObjectNotFoundException("Objeto não encontrado! Id: "+id 
@@ -32,4 +32,11 @@ public class CategoriaService {
 		return repo.save(obj);
 		
 	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+		
+	}
+	
 }
