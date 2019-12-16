3f90cc0ea23c78fd04f1894ac726fbfe6178cbd4
ArcanjoWeb <geniomatrix@hotmail>
2019-12-15 22:07:45 -0300

    Validacao customizada : email nao repetido na insercao de cliente

--- src/test/cursomc/src/main/java/com/arcanjoweb/cursomc/services/validation/ClienteInsertValidator.java
+++ src/test/cursomc/src/main/java/com/arcanjoweb/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,21 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.arcanjoweb.cursomc.domain.Cliente;
 import com.arcanjoweb.cursomc.domain.enums.TipoCliente;
 import com.arcanjoweb.cursomc.dto.ClienteNewDTO;
+import com.arcanjoweb.cursomc.repositories.ClienteRepository;
 import com.arcanjoweb.cursomc.resources.exception.FieldMessage;
 import com.arcanjoweb.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -30,6 +39,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			
 		}
 
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existe"));
+			
+		}
 		
 		// inclua os testes aqui, inserindo erros na lista
 		for (FieldMessage e : list) {
