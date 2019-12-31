6353b1a62792702d88e650226c8a8a4f2b045f28
ArcanjoWeb <geniomatrix@hotmail>
2019-12-30 22:49:53 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/test/cursomc/src/main/java/com/arcanjoweb/cursomc/config/DevConfig.java
+++ src/test/cursomc/src/main/java/com/arcanjoweb/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.arcanjoweb.cursomc.services.DBService;
+import com.arcanjoweb.cursomc.services.EmailService;
+import com.arcanjoweb.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -27,6 +29,12 @@ public class DevConfig {
 		
 		dbService.instatiateTestDatabase();
 		return true;
+		
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 		
 	}
 
--- src/test/cursomc/src/main/java/com/arcanjoweb/cursomc/services/DBService.java
+++ src/test/cursomc/src/main/java/com/arcanjoweb/cursomc/services/DBService.java
@@ -112,7 +112,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "343434334334", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Eugenio Arcanjo", "eugenio@arcanjoweb.com.br", "343434334334", TipoCliente.PESSOAFISICA);
 		
 		cli1.getTelefones().addAll(Arrays.asList("3232322323","666555455"));
 		
