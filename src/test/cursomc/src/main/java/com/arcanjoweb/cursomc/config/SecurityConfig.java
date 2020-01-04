cbc11b75ef7d1eb3514906a5005837b797c75902
ArcanjoWeb <geniomatrix@hotmail>
2020-01-03 22:45:04 -0300

    Implementando autorizacao

--- src/test/cursomc/src/main/java/com/arcanjoweb/cursomc/config/SecurityConfig.java
+++ src/test/cursomc/src/main/java/com/arcanjoweb/cursomc/config/SecurityConfig.java
@@ -20,7 +20,9 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.arcanjoweb.cursomc.security.JWTAuthenticationFilter;
+import com.arcanjoweb.cursomc.security.JWTAuthorizationFilter;
 import com.arcanjoweb.cursomc.security.JWTUtil;
+
 
 @Configuration
 @EnableWebSecurity
@@ -36,8 +38,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private JWTUtil jwtUtil;
 	
-	
-
 	private static final String[] PUBLIC_MATCHERS = {
 			"/h2-console/**"
 	};
@@ -45,7 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private static final String[] PUBLIC_MATCHERS_GET = {
 			"/produtos/**",
 			"/categorias/**",
-			"/clientes/**"
+			"/estados/**"
 	};
 
 	private static final String[] PUBLIC_MATCHERS_POST = {
@@ -67,6 +67,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
