dd134650a2d00851b70a438017b9504d546fc3b3
ArcanjoWeb <geniomatrix@hotmail>
2020-01-17 21:59:32 -0300

    Restrição de conteúdo: cliente só recupera ele mesmo

--- src/test/cursomc/src/main/java/com/arcanjoweb/cursomc/services/PedidoService.java
+++ src/test/cursomc/src/main/java/com/arcanjoweb/cursomc/services/PedidoService.java
@@ -4,8 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.amazonaws.services.licensemanager.model.AuthorizationException;
+import com.arcanjoweb.cursomc.domain.Cliente;
 import com.arcanjoweb.cursomc.domain.ItemPedido;
 import com.arcanjoweb.cursomc.domain.PagamentoComBoleto;
 import com.arcanjoweb.cursomc.domain.Pedido;
@@ -13,6 +18,7 @@ import com.arcanjoweb.cursomc.domain.enums.EstadoPagamento;
 import com.arcanjoweb.cursomc.repositories.ItemPedidoRepository;
 import com.arcanjoweb.cursomc.repositories.PagamentoRepository;
 import com.arcanjoweb.cursomc.repositories.PedidoRepository;
+import com.arcanjoweb.cursomc.security.UserSS;
 import com.arcanjoweb.cursomc.services.exceptions.ObjectNotFoundException;
 
 
@@ -72,6 +78,16 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 	
 }
