2735360c2b7ef71d14b32a0208583768a14c073a
ArcanjoWeb <geniomatrix@hotmail>
2020-01-28 18:52:25 -0300

    Usando padrao de nomes para imagens

--- src/test/cursomc/src/main/java/com/arcanjoweb/cursomc/services/ClienteService.java
+++ src/test/cursomc/src/main/java/com/arcanjoweb/cursomc/services/ClienteService.java
@@ -1,11 +1,13 @@
 package com.arcanjoweb.cursomc.services;
 
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -33,6 +35,7 @@ import com.arcanjoweb.cursomc.services.exceptions.ObjectNotFoundException;
 @Service
 public class ClienteService {
 	
+	
 	@Autowired
 	private ClienteRepository repo;
 	
@@ -45,6 +48,14 @@ public class ClienteService {
 	@Autowired
 	private S3Service s3Service;
 	
+	@Autowired
+	private ImageService imageService;
+		
+	@Value("${img.prefix.client.profile}")
+	private String prefix;
+	
+	@Value("${img.profile.size}")
+	private Integer size;
 
 		
 	public Cliente find(Integer id) {
@@ -136,14 +147,13 @@ public class ClienteService {
 			throw new AuthorizationException("Acesso negado");
 		}
 		
-		URI uri = s3Service.uploadFile(multipartFile);
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
 		
-		Cliente cli = repo.findOne(user.getId());
-		cli.setImageUrl(uri.toString());
-		repo.save(cli);
+		String fileName = prefix + user.getId() + ".jpg";
 		
-		return uri;
-	
+		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 	}
 
 }
