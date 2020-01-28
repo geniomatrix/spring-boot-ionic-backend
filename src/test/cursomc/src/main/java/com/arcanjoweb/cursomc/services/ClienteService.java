710fb805961d7600b49b1995202345427b412c32
ArcanjoWeb <geniomatrix@hotmail>
2020-01-28 18:18:37 -0300

    Salvando a URL do cliente

--- src/test/cursomc/src/main/java/com/arcanjoweb/cursomc/services/ClienteService.java
+++ src/test/cursomc/src/main/java/com/arcanjoweb/cursomc/services/ClienteService.java
@@ -129,8 +129,21 @@ public class ClienteService {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	}
-	
+
 	public URI uploadProfilePicture(MultipartFile multipartFile) {
-		return s3Service.uploadFile(multipartFile);
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		URI uri = s3Service.uploadFile(multipartFile);
+		
+		Cliente cli = repo.findOne(user.getId());
+		cli.setImageUrl(uri.toString());
+		repo.save(cli);
+		
+		return uri;
+	
 	}
+
 }
