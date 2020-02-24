b79d516df0b97171f193c1db3e071598a2a0cd16
ArcanjoWeb <geniomatrix@hotmail>
2020-02-23 22:34:43 -0300

    revisao geral

---
+++ src/test/cursomc/bin/src/test/configOK.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://www.digitaldataloja.com.br/admin/');
+define('HTTP_CATALOG', 'https://www.digitaldataloja.com.br/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://www.digitaldataloja.com.br/admin/');
+define('HTTPS_CATALOG', 'https://www.digitaldataloja.com.br/');
+
+// DIR
+define('DIR_APPLICATION', '/home2/dataloja/public_html/admin/');
+define('DIR_SYSTEM', '/home2/dataloja/public_html/system/');
+define('DIR_IMAGE', '/home2/dataloja/public_html/image/');
+define('DIR_STORAGE', '/home2/dataloja/storage/');
+define('DIR_CATALOG', '/home2/dataloja/public_html/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'dataloja_awshop');
+define('DB_PASSWORD', 'digital@2019');
+define('DB_DATABASE', 'dataloja_awshop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.arcanjoweb.com.br/');
